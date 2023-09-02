931305df3aa33fc5faae5116463160e965f8876e
onfranciis <onukwuf@gmail.com>
2023-09-02 17:26:48 +0100

    feat: initialise project

---
+++ src/config.ts
@@ -0,0 +1,9 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+const config = {
+  port: process.env.PORT || 1111,
+  dbUrl: process.env.DB_URL || "",
+};
+
+export default config;
---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import config from "./config";
+import app from "./app";
+
+mongoose
+  .connect(config.dbUrl)
+  .then(() => {
+    app.listen(config.port, () => {
+      console.log(`Server has started on port ${config.port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Connection to the database failed! \n", err);
+  });
