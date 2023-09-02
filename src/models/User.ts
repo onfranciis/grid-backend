e786775c0cf2a5971ef906b89a54f3dbbc35534c
onfranciis <onukwuf@gmail.com>
2023-09-02 23:59:07 +0100

    feat: create userSchema

---
+++ src/models/User.ts
@@ -0,0 +1,46 @@
+import { Model, model, Schema } from "mongoose";
+
+import { User } from "../types/Models.types";
+
+const userSchema = new Schema<User, Model<User>>({
+  first_name: {
+    type: String,
+    required: true,
+  },
+  last_name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  phone_number: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  service: {
+    isSuspended: {
+      type: Boolean,
+      default: false,
+    },
+    ConfirmationCode: {
+      type: String,
+    },
+    isConfirmed: { type: Boolean, default: false },
+    joined: {
+      type: Date,
+      default: Date.now(),
+    },
+  },
+});
+
+export default model("User", userSchema);
