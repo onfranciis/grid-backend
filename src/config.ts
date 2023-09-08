2d6cfd5606d0f915fcef91f8138e3c572848f1ac
onfranciis <onukwuf@gmail.com>
2023-09-08 23:46:19 +0100

    fix: update config

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,16 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+const environment = process.env.ENVIRONMENT || "development";
+
 const config = {
   port: process.env.PORT || 1111,
-  dbUrl: process.env.DB_URL || "",
   jwtSecret: process.env.DB_URL || "",
+
+  dbUrl:
+    environment == "development"
+      ? process.env.DEV_DB_URL || ""
+      : process.env.PROD_DB_URL || "",
 };
 
 export default config;
